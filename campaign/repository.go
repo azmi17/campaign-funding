fff128fff0cb3fa6f857ac15b795e2f468d7bc63
azmi17 <azmifarhan80@gmail.com>
2022-06-30 00:18:27 +0700

    Campaign Repository Final Part

--- campaign/repository.go
+++ campaign/repository.go
@@ -20,7 +20,7 @@ func (r *repository) FindAll() ([]Campaign, error) {
 
 	var campaigns []Campaign
 
-	err := r.db.Find(&campaigns).Error
+	err := r.db.Preload("CampaignImages", "campaign_images.is_primary = 1").Find(&campaigns).Error
 	if err != nil {
 		return campaigns, err
 	}
